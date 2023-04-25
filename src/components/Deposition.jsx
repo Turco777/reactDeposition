9ed4fe808c7f0fe71e17eea3e743082af625c890
Turco777 <agusalmona@gmail.com>
2023-04-25 19:35:17 -0300

    reactDeposition

---
+++ src/components/Deposition.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../style-sheets/Deposition.css';
+
+
+function Deposition(props){
+
+  return (
+    
+      <div className = 'deposition-container' >
+        <img 
+          className = 'deposition-image' 
+          src = {require(`../images/${props.image}.png`)} 
+          alt = "foto" 
+        />
+        <div className = 'deposition-text-container'>
+          <p className = 'deposition-name'>
+            <strong>{props.name}</strong> in {props.country}
+          </p>
+          <p className = 'deposition-load'>
+            {props.position} at <strong>{props.company}</strong>
+          </p>
+          <p className = 'deposition-text'>"{props.deposition}"</p>
+        </div>
+      </div>
+    
+  );
+}
+
+
+
+export default Deposition;
